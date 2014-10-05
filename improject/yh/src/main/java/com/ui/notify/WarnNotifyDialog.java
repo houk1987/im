83d3bfa02c498b9729aba5055d5d7460c5636486
houk1987 <598470593@qq.com>
2014-10-05 10:53:18 +0800

    yh

--- improject/yh/src/main/java/com/ui/notify/WarnNotifyDialog.java
+++ improject/yh/src/main/java/com/ui/notify/WarnNotifyDialog.java
@@ -6,6 +6,8 @@ import com.ui.resource.YhImageRes;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by HK on 2014/10/5.
@@ -19,20 +21,24 @@ public class WarnNotifyDialog extends JDialog {
         super(owner,title,model);
         setLocationRelativeTo(owner);
         setContentPane(new ContentPane());
-
+        getRootPane().setDefaultButton(okButton);
+        okButton.requestFocus();
     }
 
     class ContentPane extends JPanel{
         ContentPane() {
             setBackground(new Color(119,36,111));
             setLayout(null);
-
-
             JLabel loginWarnTipMessageLabel = JLabelFactory.getInstance().createJLabelWithImage(loginWarnTipMessage);
             loginWarnTipMessageLabel.setLocation(5,5);
             add(loginWarnTipMessageLabel);
-
-            okButton.setLocation(65,40);
+            okButton.setLocation(65, 40);
+            okButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dispose();
+                }
+            });
             add(okButton);
         }
     }
